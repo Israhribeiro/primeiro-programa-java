6b207ef8bb5e47b8d87999bd5d19af6cbc298b6f
Israel Tavares Ribeiro <39707610+Israhribeiro@users.noreply.github.com>
2021-08-11 19:31:14 -0300

    Arquivos do primeiro programa

---
+++ PrimeiroProjeto/src/primeiroPrograma/Principal.java
@@ -0,0 +1,16 @@
+package primeiroPrograma;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		String nome = in.nextLine();
+		
+		System.out.printf("Olá %s!\n", nome);
+
+	}
+
+}
